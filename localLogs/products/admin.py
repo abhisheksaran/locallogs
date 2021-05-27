ea4e2a861966dc1ffe979c3b9e6e40529299d961
abhisheksaran <abhishekbishnoi695@gmail.com>
2021-05-27 20:23:33 +0530

    Registerd model under admin site

--- localLogs/products/admin.py
+++ localLogs/products/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import User, Items, Orders
+
+admin.site.register(User)
+admin.site.register(Items)
+admin.site.register(Orders)
